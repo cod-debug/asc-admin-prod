2085badf779b1fded8d94c942e3bdb0aae5c4748
unknown <roy.duenas@mobilemoney.ph>
2022-12-16 23:57:22 +0800

    updates

---
+++ dist/spa/js/361.9587b3ae.js
@@ -0,0 +1,1 @@
+"use strict";(self["webpackChunkborough_fe"]=self["webpackChunkborough_fe"]||[]).push([[361],{361:(e,r,u)=>{u.r(r),u.d(r,{default:()=>f});var n=u(9835);function o(e,r){const u=(0,n.up)("router-view");return(0,n.wg)(),(0,n.j4)(u)}var t=u(1639);const s={},c=(0,t.Z)(s,[["render",o]]),f=c}}]);
---
+++ dist/spa/js/513.c2f0e06e.js
@@ -0,0 +1,1 @@
+"use strict";(self["webpackChunkborough_fe"]=self["webpackChunkborough_fe"]||[]).push([[513],{513:(e,n,t)=>{t.r(n),t.d(n,{default:()=>d});var r=t(9835);function s(e,n,t,s,a,o){const u=(0,r.up)("DetailsIndex",!0);return(0,r.wg)(),(0,r.j4)(u)}var a=t(7607);const o={data:()=>({}),components:{DetailsIndex:a.Z}};var u=t(1639);const c=(0,u.Z)(o,[["render",s]]),d=c}}]);
